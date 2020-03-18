52320aad4472fb35272e9592ffc44f3e63a083ce
Shivam Jindal <jshivam21@gmail.com>
2020-03-17 22:36:24 -0700

    included middleware for express verification

--- server/server.js
+++ server/server.js
@@ -1,9 +1,40 @@
 var express = require("express");
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+var cors = require('cors');
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+    issuer: 'https://${yourOktaDomain}/oauth2/default',
+    clientId: '{clientId}',
+    assertClaims: {
+        aud: 'api://default',
+    },
+});
+
+function authenticationRequired(req, res, next) {
+    const authHeader = req.headers.authorization || '';
+    const match = authHeader.match(/Bearer (.+)/);
+
+    if (!match) {
+        return res.status(401).end();
+    }
+
+    const accessToken = match[1];
+    const expectedAudience = 'api://default';
+
+    return oktaJwtVerifier.verifyAccessToken(accessToken, expectedAudience)
+        .then((jwt) => {
+            req.jwt = jwt;
+            next();
+        })
+        .catch((err) => {
+            res.status(401).send(err.message);
+        });
+}
 
 
 const app = express();
+app.use(cors());
 
-app.use(express.urlencoded());
 const port = 8080;
 app.listen(port);
 
