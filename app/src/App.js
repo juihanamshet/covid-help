b1819b1eb644468ad2106e80f8c2267a9fc2901a
Shivam Jindal <jshivam21@gmail.com>
2020-03-21 23:49:40 -0700

    okta flow is almost working completely

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import Auth from './Auth.js';
 import { makeStyles } from '@material-ui/core/styles';
 import { Toolbar, AppBar, Typography, Button } from '@material-ui/core';
 import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
-import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
+import { Security, LoginCallback, SecureRoute, useOktaAuth } from '@okta/okta-react';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -27,44 +27,29 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const ABOUT_US = 0;
-const FIND_OFFER = 1;
-const AUTH = 2;
 const config = {
   issuer: 'https://dev-937142.okta.com/oauth2/default',
-  redirectUri: window.location.origin + '/',
+  redirectUri: window.location.origin + '/implicit/callback',
   clientId: '0oa45qus51SlHZNDA4x6',
-  pkce: true
+  pkce: true,
+
 };
 
-function App() {
+const App = () => {
   const classes = useStyles();
-  const [page, setPage] = useState(0);
-  var childpage;
 
-  switch (page) {
-    case ABOUT_US:
-      // childpage = (<AboutUs></AboutUs>);
-      childpage = (<Route path='/' exact={true} component={AboutUs} />);
-      console.log('about us');
-      break;
-    case FIND_OFFER:
-      // childpage = (<FindOffer></FindOffer>);
-      childpage = (<SecureRoute path='/offers' exact={true} component={FindOffer} />);
+  // const { authState } = useOktaAuth;
 
-      console.log('find & offer');
-      break;
-    case AUTH:
-      // childpage = (<Auth></Auth>);
-      childpage = (<Route path='/login' exact={true} component={Auth} />);
-      console.log('auth');
-      break;
-    default:
-      childpage = (null);
-  }
+
+
+  const loginButton = <Link className={classes.tab} to="/login">Log In</Link>;
+
+  // authState.isAuthenticated ?
+  //   <Link className={classes.tab} to="/login">Log In</Link> :
+  //   <Link className={classes.tab} to="/logout">Log Out</Link>;
+
 
   return (
-
     <div className={classes.root}>
       <Router>
         <Security {...config}>
@@ -72,31 +57,30 @@ function App() {
             <Toolbar>
               <Typography variant="h5" className={classes.title}>
                 Project Student Relief
-          </Typography>
+              </Typography>
               <Link
                 className={classes.tab}
                 to="/">
                 About
-          </Link>
+              </Link>
               <Link
                 className={classes.tab}
                 to="offers">
                 Find & Offer
-          </Link>
-              <Link
-                className={classes.tab}
-                to="/login">
-                Sign In
-                </Link>
+              </Link>
+              {loginButton}
             </Toolbar>
           </AppBar>
           <Route path='/' exact={true} component={AboutUs} />
           <SecureRoute path='/offers' exact={true} component={FindOffer} />
           <Route path='/login' exact={true} component={Auth} />
+          <Route path='/implicit/callback' component={LoginCallback} />
           {/* {childpage} */}
         </Security>
       </Router>
-    </div>
+
+
+    </div >
   );
 }
 
