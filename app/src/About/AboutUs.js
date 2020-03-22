8ab74b3357852e72d06f39337231dddcf354a1c4
Shivam Jindal <jshivam21@gmail.com>
2020-03-22 15:23:14 -0700

    cleaned app.js, log in button changes to log out afterwards, find offers page redirects to log in if user not logged in

--- app/src/About/AboutUs.js
+++ app/src/About/AboutUs.js
@@ -5,6 +5,7 @@ import { withStyles } from '@material-ui/styles';
 import { Paper, Typography, Grid, Card, CardContent, CardActions, Button } from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import bgImage from '../img/neighborhood-lizSanchez.jpg';
+import NavBar from '../NavBar.js'
 
 const styling = {
     paperContainer: {
@@ -51,7 +52,9 @@ class AboutUs extends Component {
         const { classes } = this.props;
 
         return (
+
             <div>
+                <NavBar></NavBar>
                 <Paper square className={classes.banner} style={styling.paperContainer}>
                     <Grid container spacing={3}>
                         <Grid item sm={12}>
@@ -81,9 +84,9 @@ class AboutUs extends Component {
                                     <Typography align='center' variant="h4">
                                         - Our Goal -
                                     </Typography>
-                                        <br />
-                                        <Typography variant='body1'>
-                                            In response to the recent outbreak of COVID-19, many universities in the United States have closed indefintely to help mitigate the spread of the disease. While for some, this period marks an opportunity to return home to friends and family, for others, the closing of school represents a period of housing uncertainty. <br /> <br /> International students barred from returning home or local students without the resources to return home are put in a very difficult situation with no clear end in sight. COVID-19 exposes a very clear need for a network of good samaritans to help support college students in times of emergency when going home is not an option. Our goal is simple: we want to connect faculty, alumni, and fellow students with empty bedrooms or open floor space with students in need of emergency housing. <br /><br /> We hope you will join us.
+                                    <br />
+                                    <Typography variant='body1'>
+                                        In response to the recent outbreak of COVID-19, many universities in the United States have closed indefintely to help mitigate the spread of the disease. While for some, this period marks an opportunity to return home to friends and family, for others, the closing of school represents a period of housing uncertainty. <br /> <br /> International students barred from returning home or local students without the resources to return home are put in a very difficult situation with no clear end in sight. COVID-19 exposes a very clear need for a network of good samaritans to help support college students in times of emergency when going home is not an option. Our goal is simple: we want to connect faculty, alumni, and fellow students with empty bedrooms or open floor space with students in need of emergency housing. <br /><br /> We hope you will join us.
                                         </Typography>
                                 </CardContent>
                                 <CardActions className={classes.cardActions}>
---
+++ app/src/AllAppRoutes.js
@@ -0,0 +1,66 @@
+import { Security, LoginCallback, SecureRoute, useOktaAuth } from '@okta/okta-react';
+import { Toolbar, AppBar, Typography, Button } from '@material-ui/core';
+import AboutUs from './About/AboutUs.js';
+import FindOffer from './FindOffer/FindOffer.js';
+import Auth from './Auth.js';
+import React, { useState } from 'react';
+import { useHistory, Link, BrowserRouter as Router, Route } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+const AllAppRoutes = () => {
+    const useStyles = makeStyles(theme => ({
+        root: {
+            flexGrow: 1,
+        },
+        appBar: {
+            boxShadow: 'none',
+        },
+        title: {
+            flexGrow: 1,
+        },
+        tab: {
+            color: 'white',
+            "&:hover": {
+                backgroundColor: 'transparent',
+                color: '#eceff1',
+                textDecoration: 'underline',
+            }
+        }
+    }));
+    const classes = useStyles();
+
+
+
+    const loginButton = <Link className={classes.tab} to="/login">Log In</Link>;
+    // authState.isAuthenticated ?
+    //     <Link className={classes.tab} to="/login">Log In</Link> :
+    //     <Link className={classes.tab} to="/logout">Log Out</Link>;
+    const history = useHistory();
+    const onAuthRequired = () => {
+        history.push('/login');
+    };
+
+
+
+    const config = {
+        issuer: 'https://dev-937142.okta.com/oauth2/default',
+        redirectUri: window.location.origin + '/implicit/callback',
+        clientId: '0oa45qus51SlHZNDA4x6',
+        pkce: true,
+        onAuthRequired: onAuthRequired
+    };
+
+    return (
+        <Security {...config}>
+            <Route path='/' exact={true} component={AboutUs} />
+            <SecureRoute path='/offers' exact={true} component={FindOffer} />
+            <Route path='/login' exact={true} component={Auth} />
+            <Route path='/implicit/callback' component={LoginCallback} />
+        </Security >
+    )
+
+}
+
+export default AllAppRoutes
