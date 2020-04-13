5671a24fef3da40a38eee0dc2154d77f50310bdf
Shivam Jindal <jshivam21@gmail.com>
2020-04-12 19:17:51 -0700

    added new google maps autocomplete

--- app/src/FindOffer/CreateOffer.js
+++ app/src/FindOffer/CreateOffer.js
@@ -4,8 +4,12 @@ import { Dialog, DialogTitle, DialogContent, Paper, Grid, TextField, Button, Typ
 import { useInput } from '../hooks/input-hook';
 import { DropzoneArea } from 'material-ui-dropzone';
 import { withOktaAuth } from '@okta/okta-react';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 import FormData from 'form-data'
 import GooglePlacesAutocomplete, { geocodeByAddress } from 'react-google-places-autocomplete';
+import parse from 'autosuggest-highlight/parse';
+import throttle from 'lodash/throttle';
 
 import axios from 'axios';
 
@@ -32,8 +36,26 @@ const useStyles = makeStyles(theme => ({
     },
     submit: {
         color: "white",
-    }
+    },
+    icon: {
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(2),
+    },
 }));
+
+function loadScript(src, position, id) {
+    if (!position) {
+        return;
+    }
+
+    const script = document.createElement('script');
+    script.setAttribute('async', '');
+    script.setAttribute('id', id);
+    script.src = src;
+    position.appendChild(script);
+}
+
+const autocompleteService = { current: null };
 
 function CreateOffer(props) {
     const accessToken = props.authState.accessToken;
@@ -42,6 +64,9 @@ function CreateOffer(props) {
     const [lgbtq, setLgbtq] = useState(false);
     const [accessibility, setAccessibility] = useState(false);
     const [images, setImages] = useState([]);
+    const [inputValue, setInputValue] = React.useState('');
+    const [options, setOptions] = React.useState([]);
+    const loaded = React.useRef(false);
 
     const { value: listingName, bind: bindListingName } = useInput('');
     const { value: addressOne, setValue: setAddy1, bind: bindAddressOne } = useInput('');
@@ -101,6 +126,28 @@ function CreateOffer(props) {
             });
     };
 
+    if (typeof window !== 'undefined' && !loaded.current) {
+        if (!document.querySelector('#google-maps')) {
+            loadScript(
+                'https://maps.googleapis.com/maps/api/js?key=' + process.env.REACT_APP_GOOGLE_MAPS_API + '&libraries=places',
+                document.querySelector('head'),
+                'google-maps',
+            );
+        }
+
+        loaded.current = true;
+    }
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleLocationClick = (location) => {
+        geocodeByAddress(location)
+            .then(results => handleLocationAutoComplete(results[0]))
+            .catch(error => console.error(error));
+    }
+
     const handleFile = (files) => {
         setImages(files);
     }
@@ -147,6 +194,41 @@ function CreateOffer(props) {
             setAddy1(results['street_number'] + " " + results['route'])
         }
     }
+
+    const fetch = React.useMemo(
+        () =>
+            throttle((request, callback) => {
+                autocompleteService.current.getPlacePredictions(request, callback);
+            }, 200),
+        [],
+    );
+
+    React.useEffect(() => {
+        let active = true;
+
+        if (!autocompleteService.current && window.google) {
+            autocompleteService.current = new window.google.maps.places.AutocompleteService();
+        }
+        if (!autocompleteService.current) {
+            return undefined;
+        }
+
+        if (inputValue === '') {
+            setOptions([]);
+            return undefined;
+        }
+
+        fetch({ input: inputValue }, (results) => {
+            if (active) {
+                setOptions(results || []);
+            }
+        });
+
+        return () => {
+            active = false;
+        };
+    }, [inputValue, fetch]);
+
 
 
     return (
@@ -197,13 +279,57 @@ function CreateOffer(props) {
                                         // label="Address 1"
                                         type="text"
                                     /> */}
-                                    <GooglePlacesAutocomplete id="addyOne" name="addyOne"
+                                    {/* <GooglePlacesAutocomplete id="addyOne" name="addyOne"
                                         apiKey={process.env.REACT_APP_GOOGLE_MAPS_API}
                                         onSelect={({ description }) => {
                                             console.log(description);
                                             geocodeByAddress(description)
                                                 .then(results => handleLocationAutoComplete(results[0]))
                                                 .catch(error => console.error(error));
+                                        }}
+                                    /> */}
+                                    <Autocomplete
+                                        id="google-map-demo"
+                                        style={{ width: 300 }}
+                                        getOptionLabel={(option) => (typeof option === 'string' ? option : option.description)}
+                                        filterOptions={(x) => x}
+                                        options={options}
+                                        autoComplete
+                                        includeInputInList
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                label="Add a location"
+                                                variant="outlined"
+                                                fullWidth
+                                                onChange={handleChange}
+                                            />
+                                        )}
+                                        renderOption={(option) => {
+                                            const matches = option.structured_formatting.main_text_matched_substrings;
+                                            const parts = parse(
+                                                option.structured_formatting.main_text,
+                                                matches.map((match) => [match.offset, match.offset + match.length]),
+                                            );
+
+                                            return (
+                                                <Grid container alignItems="center">
+                                                    <Grid item>
+                                                        <LocationOnIcon className={classes.icon} />
+                                                    </Grid>
+                                                    <Grid item xs onClick={() => handleLocationClick(option.description)}>
+                                                        {parts.map((part, index) => (
+                                                            <span key={index} style={{ fontWeight: part.highlight ? 700 : 400 }}>
+                                                                {part.text}
+                                                            </span>
+                                                        ))}
+
+                                                        <Typography variant="body2" color="textSecondary">
+                                                            {option.structured_formatting.secondary_text}
+                                                        </Typography>
+                                                    </Grid>
+                                                </Grid>
+                                            );
                                         }}
                                     />
                                 </Grid>
